e29b3c9d31a3899d0083f91abccf3c33beb0fc33
Pavel Markitantov <markitantow@gmail.com>
2024-03-27 16:57:09 +0100

    Initial Commit

---
+++ Talent Calculator/Talent_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Talent_CalculatorApp.swift
+//  Talent Calculator
+//
+//  Created by Pavel Markitantov on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Talent_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
